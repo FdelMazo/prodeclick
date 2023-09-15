e1fe8d7a1d8cdf1a8cb2c16f6653e443ae9f5edb
Federico del Mazo <fdelmazo@fi.uba.ar>
2023-09-15 14:32:17 -0300

    A little bit of self promotion...

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,15 @@
 import { Box, Flex, Heading, Image, Link, VStack } from "@chakra-ui/react";
 import { ProdeProvider } from "../logic/ProdeContext";
 
-const FooterLink = ({ link, text, left }) => {
+const FooterLink = ({ link, text, left, title }) => {
   return (
-    <Link href={link} isExternal display="flex" alignItems="center">
+    <Link
+      href={link}
+      isExternal
+      display="flex"
+      alignItems="center"
+      title={title}
+    >
       {left}
       <Heading color="gray.600" fontSize="md" ml={2}>
         {text}
@@ -15,30 +21,48 @@ const FooterLink = ({ link, text, left }) => {
 function MainLayout(props) {
   const { children } = props;
   return (
-    <Box p={6} minH="99vh">
-      <Heading>prode.click</Heading>
-      <Heading color="gray.600" fontSize="xl">
-        Elecciones Generales 2023 🇦🇷
-      </Heading>
+    <>
+      <Box p={6} minH="99vh">
+        <Heading>prode.click</Heading>
+        <Heading color="gray.600" fontSize="xl">
+          Elecciones Generales 2023 🇦🇷
+        </Heading>
 
-      <VStack spacing={6} py={6}>
-        {children}
-      </VStack>
-
-      <Flex position="absolute" bottom={1} gap={4} alignItems="center">
-        {/* <FooterLink
-          text="source code"
-          link="https://github.com/fdelmazo/prode"
-          left={<Icon boxSize={6} as={BsGithub} />}
-        />
-        • */}
+        <VStack spacing={6} py={6}>
+          {children}
+        </VStack>
+      </Box>
+      <Flex
+        position="absolute"
+        bottom={0}
+        justifyContent="space-between"
+        alignItems="center"
+        w="100%"
+        px={2}
+      >
+        <Flex gap={4} alignItems="center">
+          {/* <FooterLink
+            text="source code"
+            link="https://github.com/fdelmazo/prode"
+            left={<Icon boxSize={6} as={BsGithub} />}
+          />
+          • */}
+          <FooterLink
+            text="fede.dm"
+            link="https://fede.dm"
+            left={
+              <Image src="img/deadmona.png" boxSize={7} display={"inline"} />
+            }
+          />
+        </Flex>
         <FooterLink
-          text="fede.dm"
-          link="https://fede.dm"
-          left={<Image src="img/deadmona.png" boxSize={7} display={"inline"} />}
+          text=""
+          title="invitame un cafecito"
+          link="https://cafecito.app/fdelmazo"
+          left={<Image src="img/cafecito.png" boxSize={9} display={"inline"} />}
         />
       </Flex>
-    </Box>
+    </>
   );
 }
 
